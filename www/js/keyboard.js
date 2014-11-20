1f22607c0d9c4f5531dd98e2a88597620b3fefa1
fabbox <fbox.dev@gmail.com>
2014-11-20 13:52:59 +0100

    version 0.1.0 : add an emoji panel !

--- www/js/keyboard.js
+++ www/js/keyboard.js
@@ -1,96 +1,131 @@
 "use strict";
+window.addEventListener('DOMContentLoaded', function () {
+    //console.log('dom content loaded');
+    (function () {
+        var inputContext = null,
+                keyboardElement = null;
 
-(function () {
-    var inputContext = null,
-        keyboardElement;
+        function init() {
+            //console.log('init');
 
-    function init() {
-        keyboardElement = document.getElementById('keyboard');
+            keyboardElement = document.getElementById('keyboard');
 
-        window.navigator.mozInputMethod.oninputcontextchange = function () {
-            inputContext = navigator.mozInputMethod.inputcontext;
-            resizeWindow();
-        };
+            window.navigator.mozInputMethod.oninputcontextchange = function () {
+                inputContext = navigator.mozInputMethod.inputcontext;
+                resizeWindow();
+            };
 
-        window.addEventListener('resize', resizeWindow);
+            window.addEventListener('resize', resizeWindow);
 
-        keyboardElement.addEventListener('mousedown', function (evt) {
-            //onMouseDown
-            // Prevent loosing focus to the currently focused app
-            // Otherwise, right after mousedown event, the app will receive a focus event.
-            evt.preventDefault();
-        });
+            keyboardElement.addEventListener('mousedown', function (evt) {
+                //onMouseDown
+                // Prevent loosing focus to the currently focused app
+                // Otherwise, right after mousedown event, the app will receive a focus event.
+                evt.preventDefault();
+            });
 
-        /* General Smiley Keys */
-        var smileyKeys = keyboardElement.querySelectorAll("button.smiley");
-        for (var i = 0; i < smileyKeys.length; i++) {
+            /* Smiley Keys */
+            var smileyKeys = keyboardElement.querySelectorAll("button.smiley");
+            for (var i = smileyKeys.length - 1; i >= 0; i--) {
+                smileyKeys[i].addEventListener('click', function (e) {
+                    var smiley = e.target.textContent;
+                    for (var j = 0; j < smiley.length; j++) {
+                        sendKey(smiley.charCodeAt(j));
+                    }
+                });
+            }
 
-            smileyKeys[i].addEventListener('click', function (e) {
-                var smiley = e.target.textContent;
-                for (var j = 0; j < smiley.length; j++) {
-                    sendKey(smiley.charCodeAt(j));
-                }
+            /* Emoji Keys */
+            var emojiKeys = keyboardElement.querySelectorAll("button.emoji");
+            for (var i = emojiKeys.length - 1; i >= 0; i--) {
+                emojiKeys[i].addEventListener('click', function (e) {
+                    var smiley = e.target.textContent;
+                    for (var j = 0; j < smiley.length; j++) {
+                        sendKey(smiley.charCodeAt(j));
+                    }
+                });
+            }
+
+            /* Special keys of the keyboards*/
+
+            var backKeys = keyboardElement.querySelectorAll("button.specKey_backspace");
+            for (var i = backKeys.length - 1; i >= 0; i--) {
+                backKeys[i].addEventListener('click', function () {
+                    sendKey(KeyEvent.DOM_VK_BACK_SPACE);
+                });
+            }
+
+            document.getElementById('specKey_enter')
+                    .addEventListener('click', function () {
+                        sendKey(KeyEvent.DOM_VK_RETURN);
+                    });
+
+            document.getElementById('specKey_space')
+                    .addEventListener('click', function () {
+                        sendKey(KeyboardEvent.DOM_VK_SPACE);
+                    });
+
+            document.getElementById('switchSmiley')
+                    .addEventListener('click', function () {
+                        if (document.getElementById('ascii').className.indexOf('hidden') >= 0) {
+                            document.getElementById('emoji').classList.add('hidden');
+                            document.getElementById('ascii').classList.remove('hidden');
+                            document.getElementById('switchEmoji').classList.remove('active');
+                            document.getElementById('switchSmiley').classList.add('active');
+                        }
+                    });
+
+            document.getElementById('switchEmoji')
+                    .addEventListener('click', function () {
+                        if (document.getElementById('emoji').className.indexOf('hidden') >= 0) {
+                            document.getElementById('ascii').classList.add('hidden');
+                            document.getElementById('emoji').classList.remove('hidden');
+                            document.getElementById('switchEmoji').classList.add('active');
+                            document.getElementById('switchSmiley').classList.remove('active');
+                        }
+                    });
+
+            var switchElement = document.getElementById('switchLayout');
+            switchElement.addEventListener('click', function () {
+                //switchHandler
+                navigator.mozInputMethod.mgmt.next();
+            });
+
+            // long press to trigger IME menu
+            var menuTimeout = 0;
+            switchElement.addEventListener('touchstart', function () {
+                menuTimeout = window.setTimeout(function () {
+                    navigator.mozInputMethod.mgmt.showAll();
+                }, 700);
+            });
+
+            switchElement.addEventListener('touchend', function () {
+                clearTimeout(menuTimeout);
             });
         }
 
-        /* the 4 special keys of the keyboards*/
-        document.getElementById('specKey_backspace')
-                .addEventListener('click', function (e) {
-                    sendKey(KeyEvent.DOM_VK_BACK_SPACE);
-                });
-
-        document.getElementById('specKey_enter')
-                .addEventListener('click', function (e) {
-                    sendKey(KeyEvent.DOM_VK_RETURN);
-                });
-
-        document.getElementById('specKey_space')
-                .addEventListener('click', function (e) {
-                    sendKey(KeyboardEvent.DOM_VK_SPACE);
-                });
-
-        var switchElement = document.getElementById('switchLayout');
-        switchElement.addEventListener('click', function () {
-            //switchHandler
-            var mgmt = navigator.mozInputMethod.mgmt;
-            mgmt.next();
-        });
-
-        // long press to trigger IME menu
-        var menuTimeout = 0;
-        switchElement.addEventListener('touchstart', function () {
-            menuTimeout = window.setTimeout(function () {
-                var mgmt = navigator.mozInputMethod.mgmt;
-                mgmt.showAll();
-            }, 700);
-        });
-
-        switchElement.addEventListener('touchend', function () {
-            clearTimeout(menuTimeout);
-        });
-    }
-
-    function resizeWindow() {
-        window.resizeTo(window.innerWidth, keyboardElement.clientHeight);
-    }
-
-    function sendKey(keyCode) {
-        switch (keyCode) {
-            case KeyEvent.DOM_VK_BACK_SPACE:
-            case KeyEvent.DOM_VK_RETURN:
-                //console.log("special key!");
-                if (inputContext) {
-                    inputContext.sendKey(keyCode, 0, 0);
-                }
-                break;
-
-            default:
-                if (inputContext) {
-                    inputContext.sendKey(0, keyCode, 0);
-                }
-                break;
+        function resizeWindow() {
+            window.resizeTo(window.innerWidth, keyboardElement.clientHeight);
         }
-    }
 
-    window.addEventListener('load', init);
-})();
+        function sendKey(keyCode) {
+            switch (keyCode) {
+                case KeyEvent.DOM_VK_BACK_SPACE:
+                case KeyEvent.DOM_VK_RETURN:
+                    //console.log("special key!");
+                    if (inputContext) {
+                        inputContext.sendKey(keyCode, 0, 0);
+                    }
+                    break;
+
+                default:
+                    if (inputContext) {
+                        inputContext.sendKey(0, keyCode, 0);
+                    }
+                    break;
+            }
+        }
+
+        window.addEventListener('load', init);
+    })();
+});
